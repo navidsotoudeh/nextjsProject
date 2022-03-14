8154ebcffbc7f91cd39f0dc615fb0a30f0fa5650
Navid <navidsotoudehmehr@gmail.com>
2022-03-14 23:49:11 +0330

    getserversideprops && getStaticProps

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,27 @@
 import Head from "next/head";
-import styles from "../styles/Layout.module.css";
+import { useRouter } from "next/router";
+import Header from "./Header";
+import Footer from "./Footer";
+import Showcase from "./Showcase";
+import styles from "@/styles/Layout.module.css";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
-        <div className={styles.container}>{children}</div>
       </Head>
+
+      <Header />
+
+      {router.pathname === "/" && <Showcase />}
+
+      <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import Layout from "../components/Layout";
-
-const Home = () => {
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+const Home = ({ events }) => {
+  console.log("events", events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Home : upcoming events </h1>
     </Layout>
   );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  console.log("events: ", events);
+  return {
+    props: { events },
+    revalidate: 1,
+  };
+}
